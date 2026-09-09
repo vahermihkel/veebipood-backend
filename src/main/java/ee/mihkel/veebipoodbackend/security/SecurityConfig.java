6e5614e3a6ff15ff190b9e9b10f81c2f1978b718
vahermihkel <65609256+vahermihkel@users.noreply.github.com>
2026-09-09 22:42:02 +0300

    Update SecurityConfig.java

--- src/main/java/ee/mihkel/veebipoodbackend/security/SecurityConfig.java
+++ src/main/java/ee/mihkel/veebipoodbackend/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173")); // Kui läheb LIVE-i siis see aadress muutub
+        config.setAllowedOrigins(List.of("https://veebipood-react-07-2026.onrender.com")); // Kui läheb LIVE-i siis see aadress muutub
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
